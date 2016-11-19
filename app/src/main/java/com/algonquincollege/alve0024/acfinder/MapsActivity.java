ab5ff9dc8c911dfaccf1d2527c4b04cf0656dee2
alpsleonardo <alpsleonardo@gmail.com>
2016-11-19 00:02:52 -0500

    FEAT: Allows user to search whatever they want

--- app/src/main/java/com/algonquincollege/alve0024/acfinder/MapsActivity.java
+++ app/src/main/java/com/algonquincollege/alve0024/acfinder/MapsActivity.java
@@ -9,9 +9,17 @@
 
 package com.algonquincollege.alve0024.acfinder;
 
+import android.content.Context;
 import android.location.Geocoder;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -19,7 +27,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import android.location.Address;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+
 
 import java.util.Locale;
 
@@ -27,6 +42,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private Geocoder mGeocoder;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -40,20 +60,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        final EditText userLocation = (EditText) findViewById(R.id.userLocation);
+
+        userLocation.setOnEditorActionListener(new OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_NULL &&
+                        event.getAction() == KeyEvent.ACTION_DOWN &&
+                        event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+
+                        MapsActivity.this.pin(userLocation.getText().toString());
+                        // Dismiss the keyboard
+                        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(userLocation.getWindowToken(), 0);
+                        userLocation.setText("");
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    /** Locate and pin locationName to the map. */
-    private void pin( String locationName ) {
+    /**
+     * Locate and pin locationName to the map.
+     */
+    private void pin(String locationName) {
         try {
-//            LatLng algonquin
-//                    = new LatLng(-34, 151);
+            // Get the location of the address provided by the user
             Address address = mGeocoder.getFromLocationName(locationName, 1).get(0);
-            LatLng ll = new LatLng( address.getLatitude(), address.getLongitude() );
-            mMap.addMarker( new MarkerOptions().position(ll).title(locationName) );
-            mMap.moveCamera( CameraUpdateFactory.newLatLng(ll) );
+            // Get the Latitude and Longitude from the address
+            LatLng ll = new LatLng(address.getLatitude(), address.getLongitude());
+            // Add a marker based on the Latitude and Longitude
+            mMap.addMarker(new MarkerOptions().position(ll).title(locationName));
+            // Show on the map with Zoom 15.0f which is street level
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ll, 15.0f));
             Toast.makeText(this, "Pinned: " + locationName, Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
-            Toast.makeText(this, "Not found: " + locationName, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Not found: " + locationName, Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -71,6 +119,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        // Locate Algonquin College Locations
         pin(PEMBROKE);
         pin(PERTH);
         pin(WOODROFFE);
@@ -79,5 +128,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //        LatLng sydney = new LatLng(-34, 151);
 //        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Maps Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
