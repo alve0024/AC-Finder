4102727a7978cf3ceec61dc85040dd191927f6f1
alpsleonardo <alpsleonardo@gmail.com>
2016-11-18 22:15:25 -0500

    Initial commit

---
+++ app/src/main/java/com/algonquincollege/alve0024/acfinder/Constants.java
@@ -0,0 +1,11 @@
+package com.algonquincollege.alve0024.acfinder;
+
+/**
+ * Created by leonardoalps on 2016-11-18.
+ */
+
+public interface Constants {
+     String PEMBROKE = "Algonquin College in the Valley";
+     String PERTH = "7 Craig Street, Perth, ON, K7H 1X7";
+     String WOODROFFE = "1385 Woodroffe Avenue, Ottawa, ON, K2G 1V8";
+}
---
+++ app/src/main/java/com/algonquincollege/alve0024/acfinder/MapsActivity.java
@@ -0,0 +1,83 @@
+/**
+ * Pin each campus of Algonquin College on a Google map.
+ *
+ * @author Leonardo Alps (alve0024)
+ * @version 1.0
+ *
+ */
+
+
+package com.algonquincollege.alve0024.acfinder;
+
+import android.location.Geocoder;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.location.Address;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, Constants {
+
+    private GoogleMap mMap;
+    private Geocoder mGeocoder;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        mGeocoder = new Geocoder(this, Locale.getDefault());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /** Locate and pin locationName to the map. */
+    private void pin( String locationName ) {
+        try {
+//            LatLng algonquin
+//                    = new LatLng(-34, 151);
+            Address address = mGeocoder.getFromLocationName(locationName, 1).get(0);
+            LatLng ll = new LatLng( address.getLatitude(), address.getLongitude() );
+            mMap.addMarker( new MarkerOptions().position(ll).title(locationName) );
+            mMap.moveCamera( CameraUpdateFactory.newLatLng(ll) );
+            Toast.makeText(this, "Pinned: " + locationName, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "Not found: " + locationName, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        pin(PEMBROKE);
+        pin(PERTH);
+        pin(WOODROFFE);
+
+        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
